0698c1b44604ee09db5249783fbd57bc6f77954f
Tal Spektor <talspektor@gmail.com>
2021-01-22 15:42:53 +0200

    Initial Commit

---
+++ Coupons/CouponsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponsApp.swift
+//  Coupons
+//
+//  Created by Tal talspektor on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CouponsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
